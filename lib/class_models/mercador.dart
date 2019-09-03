b254d89ea17fbf2e4277e850ed54459c01981079
nipodemos <nipodemos13@gmail.com>
2019-09-03 02:28:45 -0300

    excluida a pasta class_models
    tenho quase certeza que desse forma eu vou conseguir
    criar qual classe eu quiser com apenas o
    nome do handle dela

--- lib/class_models/mercador.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_web/services.dart';
-import 'package:testing_flutter_web_for_first_time/model/skills_list.dart';
-
-class Mercador {
-  Future<List<Skill>> getSkillsList() async {
-    List<Skill> skills = skillFromJson(await rootBundle.loadString('skills/MC.json'));
-    return skills;
-  }
-}
