6d8afd364c89eca05b9cbfc427c9e5f875a70823
Nipodemos <nipodemos13@gmail.com>
2019-09-07 20:18:39 -0300

    alterado estrutura do choose_classe.dart
    agora, tem uma array com a posição certinha de todas as classes
    e agora uma fica conectada a outra de forma mais bonita
    
    separei esse row num Arquivo, mas pensando bem, não precisaria separar
    futuramente  vou voltar pro mesmo arquivo

--- lib/screens/choose_classe.dart
+++ lib/screens/choose_classe.dart
@@ -1,135 +1,95 @@
 import 'package:flutter_web/material.dart';
-import 'package:testing_flutter_web_for_first_time/model/classe.dart';
-
-import 'class_skil_tree.dart';
+import 'package:testing_flutter_web_for_first_time/screens/row_of_class_evolution.dart';
 
 class ChooseYourClass extends StatelessWidget {
-  Map<String, String> classNames = {
-//    "Novice": "Aprendiz",
-//    "Crusader": "Templário",
-//    "Soul_Linker": "Espiritualista",
-//    "Magician_High": "Mago Trans",
-//    "Champion": "Mestre",
-//    "Star_Gladiator": "Mestre Taekwon",
-    "Archer": "Arqueiro",
-    "Acolyte": "Noviço",
-//    "Warlock_Trans": "Arcano Trans",
-//    "Swordsman_High": "Espadachim Trans",
-//    "Wanderer": "Cigana",
-//    "Knight": "Cavaleiro",
-    "Swordsman": "Espadachim",
-//    "Guillotine_Cross_Trans": "Sicário Trans",
-//    "Ranger": "Sentinela",
-//    "Dancer": "Odialisca",
-//    "Guillotine_Cross": "Sicário",
-//    "High_Priest": "Sumo Sacerdote",
-//    "Wanderer_Trans": "Cigana Trans",
-//    "Minstrel": "Menestrel",
-//    "Genetic": "Bioquímico",
-//    "Sniper": "Atirador de Elite",
-//    "Super_Novice": "SuperAprendiz",
-    "Thief": "Gatuno",
-//    "Paladin": "Paladino",
-//    "Blacksmith": "Ferreiro",
-//    "Minstrel_Trans": "Menestrel Trans",
-//    "Whitesmith": "Mestre-Ferreiro",
-//    "Oboro": "Oboro",
-//    "Rogue": "Arruaceiro",
-//    "Mechanic": "Mecânico",
-//    "High_Wizard": "Arquimago",
-    "Merchant": "Mercador",
-//    "Alchemist": "Alquimista",
-//    "Sura": "Shura",
-//    "Mechanic_Trans": "Mecânico Trans",
-//    "Monk": "Monge",
-//    "Hunter": "Caçador",
-//    "Gypsy": "Cigana",
-//    "Assassin_Cross": "Algoz",
-//    'Expanded_Super_Novice': 'Super Aprendiz Expandido',
-//    "Ranger_Trans": "Sentinela Trans",
-//    "Sage": "Sábio",
-//    "Bard": "Bardo",
-//    "Taekwon": "Taekwon",
-//    "Sorcerer_Trans": "Feiticeiro Trans",
-//    "Thief_High": "Gatuno Trans",
-//    "Merchant_High": "Mercador Trans",
-//    "Sorcerer": "Feiticeiro",
-//    "Stalker": "Desordeiro",
-//    "Rebellion": "Insurgente",
-//    "Genetic_Trans": "Bioquímico Trans",
-//    "Creator": "Criador",
-//    "Assassin": "Mercenário",
-//    "Archer_High": "Arqueiro Trans",
-//    "Sura_Trans": "Shura Trans",
-//    "Royal_Guard": "Guadrião Real",
-//    "Shadow_Chaser": "Renegado",
-//    "Royal_Guard_Trans": "Guadrião Real Trans",
-//    "Acolyte_High": "Noviço Trans",
-    "Magician": "Mago",
-//    "Shadow_Chaser_Trans": "Renegado Trans",
-//    "Ninja": "Ninja",
-//    "Warlock": "Arcano",
-//    "Arch_Bishop_Trans": "Arcebispo Trans",
-//    "Rune_Knight_Trans": "Cavaleiro Rúnico Trans",
-//    "Wizard": "Bruxo",
-//    "Lord_Knight": "Lorde",
-//    "Kagerou": "Kagerou",
-//    "Novice_High": "Aprendiz Trans",
-//    "Rune_Knight": "Cavaleiro Rúnico",
-//    "Arch_Bishop": "Arcebispo",
-//    "Priest": "Sacerdote",
-//    "Gunslinger": "Justiceiro",
-//    "Professor": "Professor"
-  };
+  final List classEvolution = [
+    {
+      "classe1": "Mago",
+      "classe2-1": 'Bruxo',
+      "classe2-2": 'Sábio',
+      "classe2-1-Trans": "Arquimago",
+      "classe2-2-Trans": "Professor",
+      "classe3-1": "Arcano",
+      "classe3-2": "Feiticeiro",
+    },
+    {
+      "classe1": "Gatuno",
+      "classe2-1": 'Mercenário',
+      "classe2-2": 'Arruaceiro',
+      "classe2-1-Trans": "Algoz",
+      "classe2-2-Trans": "Desordeiro",
+      "classe3-1": "Sicário",
+      "classe3-2": "Renegado",
+    },
+    {
+      "classe1": "Noviço",
+      "classe2-1": 'Sacerdote',
+      "classe2-2": 'Monge',
+      "classe2-1-Trans": "Sumo-Sacerdote",
+      "classe2-2-Trans": "Mestre",
+      "classe3-1": "Arcebispo",
+      "classe3-2": "Shura",
+    },
+    {
+      "classe1": "Arqueiro",
+      "classe2-1": 'Caçador',
+      "classe2-2": 'Bardo',
+      "classe2-3": 'Odialisca',
+      "classe2-1-Trans": "Atirador de Elite",
+      "classe2-2-Trans": "Menestrel",
+      "classe2-3-Trans": "Cigana",
+      "classe3-1": "Sentinela",
+      "classe3-2": "Trovador",
+      "classe3-3": "Musa",
+    },
+    {
+      "classe1": "Espadachim",
+      "classe2-1": 'Cavaleiro',
+      "classe2-2": 'Templário',
+      "classe2-1-Trans": "Lorde",
+      "classe2-2-Trans": "Paladino",
+      "classe3-1": "Cavaleiro Rúnico",
+      "classe3-2": "Guadião Real",
+    },
+    {
+      "classe1": "Mercador",
+      "classe2-1": 'Ferreiro',
+      "classe2-2": 'Alquimista',
+      "classe2-1-Trans": "Mestre-Ferreiro",
+      "classe2-2-Trans": "Criador",
+      "classe3-1": "Mecânico",
+      "classe3-2": "Bioquímico",
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    Map<String, String> inverso = classNames.map((k, v) {
-      return MapEntry(v, k);
-    });
-    List<String> lista = inverso.keys.toList();
-    lista.sort();
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Escolha a classe Desejada'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Escolha a sua Classe:',
-              style: TextStyle(fontSize: 36),
-            ),
-            SizedBox(height: 20),
-            ConstrainedBox(
-              constraints: BoxConstraints(maxHeight: 400, maxWidth: 600),
-              child: GridView.count(
-                crossAxisCount: 3,
-                children: <Widget>[
-                  ...lista.map((key) {
-                    return RaisedButton(
-                      child: Text(
-                        key,
-                        style: TextStyle(fontSize: 24),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return ActualGrid(nomeClasse: inverso[key]);
-                            },
-                          ),
-                        );
-                      },
-                    );
-                  }),
-                ],
+        child: SingleChildScrollView(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Escolha a sua Classe:',
+                style: TextStyle(fontSize: 36),
               ),
-            )
-          ],
+              SizedBox(height: 20),
+              ...classEvolution.map((item) {
+                return Container(
+                    margin: EdgeInsets.all(16),
+                    padding: EdgeInsets.all(16),
+                    width: 700,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red, width: 4),
+                    ),
+                    child: RowOfClassEvolution(item));
+              }),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/screens/row_of_class_evolution.dart
@@ -0,0 +1,219 @@
+import 'package:flutter_web/material.dart';
+import 'package:testing_flutter_web_for_first_time/screens/class_skil_tree.dart';
+
+final Map<String, String> englishClassNames = {
+  "Aprendiz": "Novice",
+  "Templário": "Crusader",
+  "Espiritualista": "Soul_Linker",
+  "Mago Trans": "Magician_High",
+  "Mestre": "Champion",
+  "Mestre Taekwon": "Star_Gladiator",
+  "Arqueiro": "Archer",
+  "Noviço": "Acolyte",
+  "Arcano Trans": "Warlock_Trans",
+  "Espadachim Trans": "Swordsman_High",
+  "Cigana": "Wanderer",
+  "Cavaleiro": "Knight",
+  "Espadachim": "Swordsman",
+  "Sicário Trans": "Guillotine_Cross_Trans",
+  "Sentinela": "Ranger",
+  "Odialisca": "Dancer",
+  "Sicário": "Guillotine_Cross",
+  "Sumo Sacerdote": "High_Priest",
+  "Cigana Trans": "Wanderer_Trans",
+  "Menestrel": "Minstrel",
+  "Bioquímico": "Genetic",
+  "Atirador de Elite": "Sniper",
+  "SuperAprendiz": "Super_Novice",
+  "Gatuno": "Thief",
+  "Paladino": "Paladin",
+  "Ferreiro": "Blacksmith",
+  "Menestrel Trans": "Minstrel_Trans",
+  "Mestre-Ferreiro": "Whitesmith",
+  "Oboro": "Oboro",
+  "Arruaceiro": "Rogue",
+  "Mecânico": "Mechanic",
+  "Arquimago": "High_Wizard",
+  "Mercador": "Merchant",
+  "Alquimista": "Alchemist",
+  "Shura": "Sura",
+  "Mecânico Trans": "Mechanic_Trans",
+  "Monge": "Monk",
+  "Caçador": "Hunter",
+  "Cigana": "Gypsy",
+  "Algoz": "Assassin_Cross",
+  "Super Aprendiz Expandido": "Expanded_Super_Novice",
+  "Sentinela Trans": "Ranger_Trans",
+  "Sábio": "Sage",
+  "Bardo": "Bard",
+  "Taekwon": "Taekwon",
+  "Feiticeiro Trans": "Sorcerer_Trans",
+  "Gatuno Trans": "Thief_High",
+  "Mercador Trans": "Merchant_High",
+  "Feiticeiro": "Sorcerer",
+  "Desordeiro": "Stalker",
+  "Insurgente": "Rebellion",
+  "Bioquímico Trans": "Genetic_Trans",
+  "Criador": "Creator",
+  "Mercenário": "Assassin",
+  "Arqueiro Trans": "Archer_High",
+  "Shura Trans": "Sura_Trans",
+  "Guadrião Real": "Royal_Guard",
+  "Renegado": "Shadow_Chaser",
+  "Guadrião Real Trans": "Royal_Guard_Trans",
+  "Noviço Trans": "Acolyte_High",
+  "Mago": "Magician",
+  "Renegado Trans": "Shadow_Chaser_Trans",
+  "Ninja": "Ninja",
+  "Arcano": "Warlock",
+  "Arcebispo Trans": "Arch_Bishop_Trans",
+  "Cavaleiro Rúnico Trans": "Rune_Knight_Trans",
+  "Bruxo": "Wizard",
+  "Lorde": "Lord_Knight",
+  "Kagerou": "Kagerou",
+  "Aprendiz Trans": "Novice_High",
+  "Cavaleiro Rúnico": "Rune_Knight",
+  "Arcebispo": "Arch_Bishop",
+  "Sacerdote": "Priest",
+  "Justiceiro": "Gunslinger",
+  "Professor": "Professor"
+};
+
+class RowOfClassEvolution extends StatelessWidget {
+  final Map<String, String> classTree;
+  RowOfClassEvolution(this.classTree);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ConstrainedBox(
+          constraints: BoxConstraints(minWidth: 150),
+          child: RaisedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return ActualGrid(nomeClasse: englishClassNames[classTree['classe1']]);
+                  },
+                ),
+              );
+            },
+            child: Text(classTree['classe1']),
+          ),
+        ),
+        SizedBox(width: 8),
+        Column(
+          children: <Widget>[
+            ConstrainedBox(
+              constraints: BoxConstraints(minWidth: 150),
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return ActualGrid(nomeClasse: englishClassNames[classTree['classe2-1']]);
+                      },
+                    ),
+                  );
+                },
+                child: Text(classTree['classe2-1']),
+              ),
+            ),
+            ConstrainedBox(
+              constraints: BoxConstraints(minWidth: 150),
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return ActualGrid(nomeClasse: englishClassNames[classTree['classe2-2']]);
+                      },
+                    ),
+                  );
+                },
+                child: Text(classTree['classe2-2']),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(width: 8),
+        Column(
+          children: <Widget>[
+            ConstrainedBox(
+              constraints: BoxConstraints(minWidth: 150),
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return ActualGrid(nomeClasse: englishClassNames[classTree['classe2-1-Trans']]);
+                      },
+                    ),
+                  );
+                },
+                child: Text(classTree['classe2-1-Trans']),
+              ),
+            ),
+            ConstrainedBox(
+              constraints: BoxConstraints(minWidth: 150),
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return ActualGrid(nomeClasse: englishClassNames[classTree['classe2-2-Trans']]);
+                      },
+                    ),
+                  );
+                },
+                child: Text(classTree['classe2-2-Trans']),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(width: 8),
+        Column(
+          children: <Widget>[
+            ConstrainedBox(
+              constraints: BoxConstraints(minWidth: 150),
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return ActualGrid(nomeClasse: englishClassNames[classTree['classe3-1']]);
+                      },
+                    ),
+                  );
+                },
+                child: Text(classTree['classe3-1']),
+              ),
+            ),
+            ConstrainedBox(
+              constraints: BoxConstraints(minWidth: 150),
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return ActualGrid(nomeClasse: englishClassNames[classTree['classe3-2']]);
+                      },
+                    ),
+                  );
+                },
+                child: Text(classTree['classe3-2']),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
