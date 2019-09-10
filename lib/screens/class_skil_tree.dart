2b8fb17ceeec0a62f229b79e1e5f3d9dda5031ff
nipodemos <nipodemos13@gmail.com>
2019-09-10 05:21:54 -0300

    major changes na class_skill_tree.dart
    parei de usar grid pra fazer o layout
    agora to usando uma column de rows
    agora tambem to checando se a skill é de quest
    se for não renderizo o botão de + e -
    e já deixo no lvl max

--- lib/screens/class_skil_tree.dart
+++ lib/screens/class_skil_tree.dart
@@ -1,24 +1,26 @@
 import 'package:flutter_web/material.dart';
-import 'package:testing_flutter_web_for_first_time/model/classe.dart';
+import 'package:flutter_web/services.dart';
+import '../models/classe.dart';
 
 Map<String, int> distribuicao = {};
 
-class ActualGrid extends StatefulWidget {
+class SkillTreeContainer extends StatefulWidget {
   final String nomeClasse;
-  ActualGrid({@required this.nomeClasse, int grau}) : assert(nomeClasse != null);
+  final bool inherit;
+  SkillTreeContainer({@required this.nomeClasse, int grau, this.inherit = false}) : assert(nomeClasse != null);
   @override
-  _ActualGridState createState() => _ActualGridState();
+  _SkillTreeContainerState createState() => _SkillTreeContainerState();
 }
 
-class _ActualGridState extends State<ActualGrid> {
-  Future<List<Skill>> buildList(String className) async {
+class _SkillTreeContainerState extends State<SkillTreeContainer> {
+  Future<List<Skill>> buildList(String className, {bool inheritOnlyOnce = false}) async {
     List<Skill> finalList = List<Skill>.generate(42, (_) {
       return Skill();
     });
     print('lista com skill vazias geradas');
     Classe playerClass = classeFromJson(await rootBundle.loadString('skills/' + className + '.json'));
     List<Skill> skills = playerClass.skills;
-    print('criada a outra list de skills');
+    print(playerClass);
     for (var i = 0; i < skills.length; i++) {
       if (skills[i].position != null) {
         finalList[skills[i].position as int] = skills[i];
@@ -27,54 +29,95 @@ class _ActualGridState extends State<ActualGrid> {
         print('skill ${skills[i].displayName} will not be shown');
       }
     }
+    if (widget.inherit) finalList = await buildSecondList(finalList, playerClass.inherit[0]);
     print('tudo terminado');
     return finalList;
   }
 
-  @override
-  void initState() {
-    super.initState();
+  Future<List<Skill>> buildSecondList(List<Skill> list, String className) async {
+    Classe playerClass = classeFromJson(await rootBundle.loadString('assets/skills/' + className + '.json'));
+    List<Skill> skills = playerClass.skills;
+    print(playerClass);
+    for (var i = 0; i < skills.length; i++) {
+      if (skills[i].position != null) {
+        list[skills[i].position as int] = skills[i];
+        print('skill ${skills[i].displayName} will show normally');
+      } else {
+        print('skill ${skills[i].displayName} will not be shown');
+      }
+    }
+    return list;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Grid"),
+        title: Text(widget.nomeClasse),
       ),
-      body: Center(
-        child: FutureBuilder(
-          future: buildList(widget.nomeClasse),
-          builder: (BuildContext context, AsyncSnapshot<List<Skill>> snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Text('Erro: ${snapshot.error}');
-                //throw snapshot.error;
-              } else if (snapshot.hasData) {
-                return GridView.count(
-                  crossAxisCount: 7,
+      body: FutureBuilder(
+        future: buildList(widget.nomeClasse, inheritOnlyOnce: true),
+        builder: (BuildContext context, AsyncSnapshot<List<Skill>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Text('Erro: ${snapshot.error}');
+              //throw snapshot.error;
+            } else if (snapshot.hasData) {
+              return SingleChildScrollView(
+                child: Column(
                   children: <Widget>[
-                    ...snapshot.data.map((skill) => SkillTile(skill)).toList(),
+                    Row(
+                      children: <Widget>[
+                        ...snapshot.data.take(7).map((value) => SkillTile(value)),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        ...snapshot.data.skip(7).take(7).map((value) => SkillTile(value)),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        ...snapshot.data.skip(14).take(7).map((value) => SkillTile(value)),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        ...snapshot.data.skip(21).take(7).map((value) => SkillTile(value)),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        ...snapshot.data.skip(28).take(7).map((value) => SkillTile(value)),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        ...snapshot.data.skip(35).take(7).map((value) => SkillTile(value)),
+                      ],
+                    ),
                   ],
-                );
-              } else {
-                return Text(
-                  'já terminou o Future mas deu ruim',
-                  style: TextStyle(fontSize: 48),
-                );
-              }
+                ),
+              );
             } else {
-              return Column(
+              return Text(
+                'já terminou o Future mas deu ruim',
+                style: TextStyle(fontSize: 48, color: Colors.red),
+              );
+            }
+          } else {
+            return Center(
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text('Aguardando dados'),
+                  Text('Aguardando dados', style: TextStyle(fontSize: 48, color: Colors.blueAccent)),
                   CircularProgressIndicator(),
                 ],
-              );
-            }
-          },
-        ),
+              ),
+            );
+          }
+        },
       ),
     );
   }
@@ -93,88 +136,105 @@ class _SkillTileState extends State<SkillTile> {
   Widget build(BuildContext context) {
     distribuicao[widget.skill.handleName] ??= 0;
     return Container(
-      margin: const EdgeInsets.all(8),
+      height: 104,
+      width: 130,
+      padding: const EdgeInsets.all(4),
       decoration: BoxDecoration(border: Border.all(width: 2)),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: widget.skill.displayName != null
-            ? Column(
-                children: <Widget>[
-                  Container(
-                    height: tamanhoDaFonte * 3,
-                    child: Center(
-                      child: Text(
-                        widget.skill.displayName,
-                        maxLines: 3,
-                        textAlign: TextAlign.center,
-                      ),
+      child: widget.skill.displayName != null
+          ? Column(
+              children: <Widget>[
+                Container(
+                  height: tamanhoDaFonte * 2,
+                  child: Center(
+                    child: Text(
+                      widget.skill.displayName,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
                     ),
                   ),
-                  Image.asset(
-                    'skills_icons/${widget.skill.handleName.toLowerCase()}.gif',
-                    height: 30,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        height: 30,
-                        width: 30,
-                        child: InkWell(
-                          onTap: distribuicao[widget.skill.handleName] > 0
-                              ? () {
-                                  if (distribuicao[widget.skill.handleName] > 1) {}
-
-                                  if (distribuicao[widget.skill.handleName] > 0) {
-                                    setState(() {
-                                      distribuicao[widget.skill.handleName]--;
-                                    });
-                                  }
-                                }
-                              : null,
-                          child: Card(
-                            child: Center(
-                              child: Text(
-                                '-',
-                                style: TextStyle(fontSize: 18),
+                ),
+                Image.asset(
+                  'skills_icons/${widget.skill.handleName.toLowerCase()}.gif',
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    widget.skill.skillType != "0x1"
+                        ? Container(
+                            height: 30,
+                            width: 30,
+                            child: InkWell(
+                              onTap: distribuicao[widget.skill.handleName] > 0
+                                  ? () {
+                                      if (distribuicao[widget.skill.handleName] > 0) {
+                                        setState(() {
+                                          distribuicao[widget.skill.handleName]--;
+                                        });
+                                      }
+                                    }
+                                  : null,
+                              child: Card(
+                                child: Center(
+                                  child: Text(
+                                    '-',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                ),
+                              ),
+                            ))
+                        : Container(),
+                    SizedBox(width: 4),
+                    Container(
+                      height: 30,
+                      width: 30,
+                      child: Center(
+                        child: Text(
+                            widget.skill.skillType != "0x1"
+                                ? distribuicao[widget.skill.handleName].toString()
+                                : widget.skill.maxLevel.toString(),
+                            style: TextStyle(fontSize: 18)),
+                      ),
+                    ),
+                    SizedBox(width: 4),
+                    widget.skill.skillType != "0x1"
+                        ? Container(
+                            height: 30,
+                            width: 30,
+                            child: InkWell(
+                              onTap: distribuicao[widget.skill.handleName] < widget.skill.maxLevel
+                                  ? () {
+                                      if (distribuicao[widget.skill.handleName] < widget.skill.maxLevel) {
+                                        if (widget.skill.preRequisites != null) {
+                                          for (Map<String, int> i in widget.skill.preRequisites) {
+                                            i.forEach((key, value) {
+                                              distribuicao[key] = value;
+                                            });
+                                          }
+                                        }
+                                        setState(() {
+                                          distribuicao[widget.skill.handleName]++;
+                                        });
+                                      }
+                                    }
+                                  : null,
+                              child: Card(
+                                child: Center(
+                                  child: Text(
+                                    '+',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 5),
-                      Text(distribuicao[widget.skill.handleName].toString(), style: TextStyle(fontSize: 18)),
-                      SizedBox(width: 5),
-                      Container(
-                        height: 30,
-                        width: 30,
-                        child: InkWell(
-                          onTap: distribuicao[widget.skill.handleName] < widget.skill.maxLevel
-                              ? () {
-                                  if (distribuicao[widget.skill.handleName] < widget.skill.maxLevel) {
-                                    setState(() {
-                                      distribuicao[widget.skill.handleName]++;
-                                    });
-                                  }
-                                }
-                              : null,
-                          child: Card(
-                            child: Center(
-                              child: Text(
-                                '+',
-                                style: TextStyle(fontSize: 18),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              )
-            : Text(''),
-      ),
+                          )
+                        : Container(),
+                  ],
+                ),
+              ],
+            )
+          : Text(''),
     );
   }
 }
