72c104215f96fb74000852cac821654412300af0
nipodemos <nipodemos13@gmail.com>
2019-09-10 20:38:01 -0300

    agora o repositório gera código pra android, windows e web :D
    pra isso acontecer, eu tive que mudar tudo
    adicionado a pasta android
    adicionado a pasta windows
    modificado os arquivos padrão pra ficar a par da mudança

---
+++ android/app/src/main/kotlin/com/example/ragnarok_skill_simulator_flutter/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ragnarok_skill_simulator_flutter
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
